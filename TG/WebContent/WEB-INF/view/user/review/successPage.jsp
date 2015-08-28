0d2851f3e2d3abea49ef7e04369fd20a66c0d3fa
joewkd <joewkd@naver.com>
2015-08-28 10:33:32 +0900

    ㅡㅡ

--- TG/WebContent/WEB-INF/view/user/review/successPage.jsp
+++ TG/WebContent/WEB-INF/view/user/review/successPage.jsp
@@ -1,12 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
-<title>리뷰등록완료</title>
-</head>
-<body>
-	
-</body>
-</html>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+${reviewId }
+<option value="함께한 친구들">함께한 친구들</option>
+		<option value="${host }">${host }</option>	
+		<c:forEach items="${list }" var="gx">
+			<option value="${gx.pid }">${gx.pid }</option>
+		</c:forEach>
--- TG/src/com/tg/common/beans/ReviewBean.java
+++ TG/src/com/tg/common/beans/ReviewBean.java
@@ -5,55 +5,29 @@ public class ReviewBean {
 	private int gno;
 	private String id;
 	private String evaluator;
-	private String comment;
+	private String evaluate;
 	private int grade;
 	
 	public ReviewBean() {
 	
 	}
-	
+
 	public ReviewBean(int reviewNo, int gno, String id, String evaluator,
-			String comment, int grade) {
-		super();
+			String evaluate, int grade) {
 		this.reviewNo = reviewNo;
 		this.gno = gno;
 		this.id = id;
 		this.evaluator = evaluator;
-		this.comment = comment;
+		this.evaluate = evaluate;
 		this.grade = grade;
 	}
-
-
 
 	public int getReviewNo() {
 		return reviewNo;
 	}
+
 	public void setReviewNo(int reviewNo) {
 		this.reviewNo = reviewNo;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getEvaluator() {
-		return evaluator;
-	}
-	public void setEvaluator(String evaluator) {
-		this.evaluator = evaluator;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public int getGrade() {
-		return grade;
-	}
-	public void setGrade(int grade) {
-		this.grade = grade;
 	}
 
 	public int getGno() {
@@ -62,6 +36,40 @@ public class ReviewBean {
 
 	public void setGno(int gno) {
 		this.gno = gno;
-	}	
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEvaluator() {
+		return evaluator;
+	}
+
+	public void setEvaluator(String evaluator) {
+		this.evaluator = evaluator;
+	}
+
+	public String getEvaluate() {
+		return evaluate;
+	}
+
+	public void setEvaluate(String evaluate) {
+		this.evaluate = evaluate;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	
 	
 }
