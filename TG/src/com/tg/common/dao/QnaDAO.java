f80ca92a1a99695400b3c5051c6dd113bb75146e
joewkd <joewkd@naver.com>
2015-08-29 10:48:45 +0900

    mm

--- TG/src/com/tg/common/dao/QnaDAO.java
+++ TG/src/com/tg/common/dao/QnaDAO.java
@@ -1,67 +1,47 @@
 package com.tg.common.dao;
 
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 
-import com.ibatis.sqlmap.client.SqlMapClient;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.tg.common.beans.QnaBean;
-import com.tg.iba.MySqlMapConfig;
 
 public class QnaDAO {
-	SqlMapClient smc;
+	@Autowired
+	SqlSession session;
 	
 	public QnaDAO() {
-		smc = MySqlMapConfig.getSqlMapInstance();
 	}
 	
 	public boolean qnaInsert(QnaBean bean){//문의하기
-		try {
-			smc.insert("qna.QnaInsert", bean);
+		int t = session.insert("qna.QnaInsert", bean);
+		if(t==1){
 			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
 		return false;
 	}
 	
 	public List<QnaBean> qnaList(String qstatus){
-		List<QnaBean> list = null;
-		try {
-			list = smc.queryForList("qna.QnaList", qstatus);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		List<QnaBean> list = session.selectList("qna.QnaList", qstatus);
 		return list;
 	}
 	
 	public QnaBean QnaSelect(int qno){
-		QnaBean bean = null;
-		try {
-			 bean = (QnaBean) smc.queryForObject("qna.QnaSelect", qno);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		QnaBean bean = session.selectOne("qna.QnaSelect", qno);
 		return bean;
 	}
 	
 	public boolean QnaReply(QnaBean bean){
-		try {
-			int t = smc.update("qna.QnaReply", bean);
-			if(t==1) return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+			int t = session.update("qna.QnaReply", bean);
+			if(t==1) {
+				return true;
+			}
 		return false;
 	}
 	
 	public List<QnaBean> QnaReplyConfirm(String id){
-		List<QnaBean> list = null;
-		try {
-			list = smc.queryForList("qna.QnaReplyConfirm", id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		List<QnaBean> list = session.selectList("qna.QnaReplyConfirm", id);
 		return list;
 	}
 }
