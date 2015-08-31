8fa7a36e28ae5cbc69be78fa9490e044008d9c8d
jomj3593 <gkckamf@naver.com>
2015-08-31 12:01:25 +0900

    명준 : (이메일 인증 후 회원가입으로) 회원가입 프로세스 변경

--- TG/src/com/tg/join/control/JoinControl.java
+++ TG/src/com/tg/join/control/JoinControl.java
@@ -15,12 +15,25 @@ public class JoinControl{
 	JoinDAO dao;
 	
 	@RequestMapping("/joinpass")
-	public String memberJoin(Model model, MemberBean bean, @RequestParam(value="id")String id){
+	public String memberJoin(Model model, MemberBean bean, 
+								@RequestParam(value="userKey") String userKey, 
+								@RequestParam(value="correctKey") String correctKey) {
 		
-		if(dao.checkJoin(id)){
+		// userKey는 유저가 입력한 인증키, correctKey는 정확한 인증키
+		
+		System.out.println("입력값 : "+userKey);
+		System.out.println("받은값 : "+correctKey);
+		
+		if(dao.checkJoin(bean.getId())){ // 이미 사용중인 아이디 체크
 			model.addAttribute("join", "fail");
 			
-		}else{
+		} else if(!userKey.equals(correctKey)) { // 인증키 체크
+			model.addAttribute("join", "keyFail");
+		} else {
+			System.err.println("여기까지 도달");
+			System.out.println(bean.getId());
+			System.out.println("패스"+bean.getPass());
+			System.out.println(bean.getNick());
 			dao.join(bean);
 			model.addAttribute("join", "success");
 		}
