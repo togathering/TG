f7e0df2e9d97947d494d02dcdcf44a601d1aeb70
joewkd <joewkd@naver.com>
2015-09-01 10:00:27 +0900

    mm

--- TG/src/com/tg/common/dao/MemberDAO.java
+++ TG/src/com/tg/common/dao/MemberDAO.java
@@ -1,29 +1,22 @@
 package com.tg.common.dao;
 
 
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.ibatis.sqlmap.client.SqlMapClient;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.tg.common.beans.MemberBean;
 import com.tg.common.beans.MemberBeanIn;
-import com.tg.iba.MySqlMapConfig;
-import com.tg.email.model.Email;
 
 public class MemberDAO {
-	SqlMapClient smc;
-	String nothing;
-	public int totalline;
-	public int endline;
-	public int startline;
+	@Autowired
+	SqlSession session;
+	
 	public MemberDAO() {
-		smc = MySqlMapConfig.getSqlMapInstance();
-		nothing = "m.id";
-		totalline=selectCnt();
-		startline=1;
-		endline= 10;
+	
 	}
 	
 
@@ -32,114 +25,70 @@ public class MemberDAO {
 	
 	public String getMemberTel(String id){
 		String tel = "";
-		try {
-			tel =  (String) smc.queryForObject("member.telInfo", id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		tel =  session.selectOne("member.telInfo", id);
 		return tel;
 	}
 	
 	public List<MemberBean> mSelectAll(){
-		List<MemberBean> list = null;
-		try {
-			list=smc.queryForList("member.MselectAll");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		List<MemberBean> list = session.selectList("member.MselectAll");
 		return list;
 	}
 	public List<MemberBean> selectPage(int start, int end){
-		List<MemberBean> list = null;
-		  try {
 			 HashMap<Object, Object> map = new HashMap<Object,Object >();
 			 String id = "%@%";
 			 map.put("s", start);
 			 map.put("e", end);
 			 map.put("id",id);
-			  list =(List<MemberBean>)smc.queryForList("member.MselectAll", map);
+			 List<MemberBean> list = session.selectList("member.MselectAll", map);
 		
-		  } catch (SQLException e) {
-				e.printStackTrace();
-			}
-				return list;
+			return list;
 	   }//selectPage
 	
 	public List<MemberBean> mSelectId(String id){
-		List<MemberBean> list = null;
 		Map<Object, Object> map = new HashMap<Object, Object>();
 		id="%"+id+"%";
-		System.out.println("dao"+id);
 		map.put("s",1);
 		map.put("e", 500);	
 		map.put("id",id );
-		try {
-			list=smc.queryForList("member.MselectAll",map);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		List<MemberBean> list = session.selectList("member.MselectAll",map);
+
 		return list;
 	}
 	public int selectCnt(){
-		int cnt = 0;
-			try {
-				cnt =(int)smc.queryForObject("member.memberCnt");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		int cnt = session.selectOne("member.memberCnt");
 		return cnt;
 	}
 	
 	public List<MemberBean> searchMember(String search, String id){
-		List<MemberBean> list = null;
-		search = "%"+search+"%";
-		
+		search = "%"+search+"%";		
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("search", search);
 		map.put("id", id);
-		try {
-			list = smc.queryForList("member.searchMember",map);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		List<MemberBean> list = session.selectList("member.searchMember",map);
+		
 		return list;
 	}
 	
 	public MemberBean selectMem(String id){
-		MemberBean bean = null;
-		try {
-			bean = (MemberBean) smc.queryForObject("member.selectMem",id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		MemberBean bean = session.selectOne("member.selectMem",id);
 		return bean;
 	}
 	public MemberBeanIn profile(String id){
-		MemberBeanIn my = null;
-		try {
-			my = (MemberBeanIn) smc.queryForObject("member.my", id);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		MemberBeanIn my = session.selectOne("member.my", id);
 		return my;
 	}
 	public boolean upProfile(MemberBeanIn my){
-		try {
-			smc.update("member.profile", my);
+		int t = session.update("member.profile", my);
+		if(t==1){
 			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
 		return false;
 	}
 	
 	public String getPass(String id){
-		try {
-			String pass = (String) smc.queryForObject("member.getPass", id);
-			return pass;
-		
-		} catch (SQLException e) {
-			e.printStackTrace();
+		String pass = session.selectOne("member.getPass", id);
+		if(pass != null){
+			return pass;			
 		}
 		return "fail";
 	}
