0a040d73ed32d9ca02bf584cb8152271115de400
joewkd <joewkd@naver.com>
2015-09-03 16:36:09 +0900

    bb

--- TG/src/com/tg/common/dao/GroupPreferenceDAO.java
+++ TG/src/com/tg/common/dao/GroupPreferenceDAO.java
@@ -1,5 +1,6 @@
 package com.tg.common.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -16,5 +17,22 @@ public class GroupPreferenceDAO {
 		List<GroupPreferenceBean> list = session.selectList("group_pre.selectAll");
 		
 		return list;
+	}
+	
+	public Integer selectRatio(int gno){
+		int ratio = session.selectOne("group_pre.selectRatio",gno);
+		return ratio;
+	}
+	
+	public boolean upRatio(int ratio, int gno){
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("ratio", ratio);
+		map.put("gno", gno);
+		
+		int t = session.update("group_pre.upRatio", map);
+		if(t==1){
+			return true;
+		}
+		return false;
 	}
 }
--- TG/src/com/tg/group/join/control/GroupJoinControl.java
+++ TG/src/com/tg/group/join/control/GroupJoinControl.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.tg.common.beans.GroupBean;
 import com.tg.common.beans.ParticipantBean;
 import com.tg.common.dao.GroupDAO;
+import com.tg.common.dao.GroupPreferenceDAO;
 import com.tg.common.dao.InviteGroupDAO;
+import com.tg.common.dao.MemberDAO;
 import com.tg.common.dao.ParticipantDAO;
 
 @Controller
@@ -21,6 +23,10 @@ public class GroupJoinControl {
 	ParticipantDAO pdao;
 	@Autowired
 	InviteGroupDAO idao;
+	@Autowired
+	GroupPreferenceDAO gpdao;
+	@Autowired
+	MemberDAO mdao;
 
 	@RequestMapping("/joingroup")
 	public String groupJoin(@RequestParam(value="gno")String gxno, HttpSession session){
@@ -39,10 +45,19 @@ public class GroupJoinControl {
 			gstatus = "모집완료";
 		}else{
 			gstatus = "모집 중";
-		}		
+		}
 		
 		if(gdao.joinG(gsum, gno, gstatus) && pdao.joinGroup(gno, id)){
-			System.out.println("참여 성공");						
+			System.out.println("참여 성공");
+			int ratio = gpdao.selectRatio(gno);
+			String gender = mdao.selectMem(id).getGender();
+			if(gender.equals("M")){
+				ratio = ratio+1;
+			}else{
+				ratio = ratio-1;
+			}
+			gpdao.upRatio(ratio, gno);
+			
 		}else{
 			System.out.println("참여 실패");			
 		}
@@ -62,7 +77,15 @@ public class GroupJoinControl {
 		int gsum = gbean.getGsum();
 		gsum--;
 		if(gdao.joinG(gsum, gno, "모집 중") &&	pdao.cancelJoin(pbean)){
-			System.out.println("참여취소 성공");				
+			System.out.println("참여취소 성공");
+			int ratio = gpdao.selectRatio(gno);
+			String gender = mdao.selectMem(id).getGender();
+			if(gender.equals("M")){
+				ratio = ratio-1;
+			}else{
+				ratio = ratio+1;
+			}
+			gpdao.upRatio(ratio, gno);
 		}else{
 			System.out.println("참여취소 실패");				
 		}
