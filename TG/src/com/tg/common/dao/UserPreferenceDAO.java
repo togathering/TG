51c6b514c4d2fc89b9274a5cbf2effea13e43e96
joewkd <joewkd@naver.com>
2015-09-03 10:42:23 +0900

    nn

---
+++ TG/src/com/tg/common/dao/UserPreferenceDAO.java
@@ -0,0 +1,42 @@
+package com.tg.common.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tg.common.beans.UserPreferenceBean;
+
+public class UserPreferenceDAO {
+	@Autowired
+	SqlSession session;
+	
+	public UserPreferenceBean select(String id){
+		
+		UserPreferenceBean bean = session.selectOne("user_pre.select", id);
+		
+		return bean;
+	}
+	
+	// 선호도 제출
+	public boolean insert(UserPreferenceBean bean){
+		int t = session.insert("user_pre.insert", bean);
+		if(t==1){
+			return true;			
+		}
+		return false;
+	}
+	
+	public String selectTopProvince(String id){
+		String topProvince = session.selectOne("user_pre.topProvince",id);
+		
+		return topProvince;
+	}
+	
+	public String selectTopCategory(String id){
+		String topCategory = session.selectOne("user_pre.topCategory",id);
+		return topCategory;
+	}
+	
+	public int selectRatioSum(String id){
+		return session.selectOne("user_pre.ratioSum", id);
+	}
+}
